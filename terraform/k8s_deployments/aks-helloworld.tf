0095bbe8ccedf62473d4f312fc866a1991977a54
Michael Oberdorf <michael.oberdorf@bridging-it.de>
2021-07-06 13:22:42 +0200

    define hello world app

---
+++ terraform/k8s_deployments/aks-helloworld.tf
@@ -0,0 +1,84 @@
+# https://codeberg.org/hjacobs/kube-ops-view/src/branch/main/deploy/rbac.yaml
+resource "kubernetes_deployment" "aks-helloworld" {
+  provider = "kubernetes.azure_k8s"
+
+  metadata {
+    name = "aks-helloworld"
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "aks-helloworld"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "aks-helloworld"
+        }
+      }
+      spec {
+        container {
+          image = "mcr.microsoft.com/azuredocs/aks-helloworld:v1"
+          name  = "aks-helloworld"
+
+          port {
+            container_port = "80"
+            protocol = "TCP"
+          }
+          
+          env {
+            "TITLE" = "Welcome to Azure Kubernetes Service (AKS)"
+          }
+        }
+      }
+    }
+  }
+}
+resource "kubernetes_service" "aks-helloworld" {
+  provider = "kubernetes.azure_k8s"
+
+  metadata {
+    name = "aks-helloworld"
+  }
+  spec {
+    selector = {
+      app = "aks-helloworld"
+    }
+    port {
+      port        = 80
+    }
+    type = "ClusterIP"
+  }
+}
+
+
+resource "kubernetes_ingress" "aks-helloworld" {
+  provider = "kubernetes.azure_k8s"
+  
+  metadata {
+    name = "aks-helloworld"
+    annotations = {
+      "kubernetes.io/ingress.class" = "addon-http-application-routing"
+    }
+  }
+
+  spec {
+    rule {
+      http {
+        path {
+          backend {
+            service_name = "kube-ops-view"
+            service_port = 80
+          }
+
+          path = "/"
+        }
+      }
+    }
+  }
+}
