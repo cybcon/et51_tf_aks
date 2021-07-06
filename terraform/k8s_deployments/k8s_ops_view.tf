1b7f3f81733b73581008aa8a569b56d145d22851
Michael Oberdorf <michael.oberdorf@bridging-it.de>
2021-07-06 13:23:14 +0200

    move kubernetes provider configuration to an own file

--- terraform/k8s_deployments/k8s_ops_view.tf
+++ terraform/k8s_deployments/k8s_ops_view.tf
@@ -1,16 +1,3 @@
-provider "kubernetes" {
-    host                   = azurerm_kubernetes_cluster.azure_k8s.kube_config.0.host
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.azure_k8s.kube_config.0.client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.azure_k8s.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.azure_k8s.kube_config.0.cluster_ca_certificate)
-    alias                  = "azure_k8s"
-
-    # config_path    = "~/.kube/config"
-    config_context = azurerm_kubernetes_cluster.azure_k8s.name
-}
-
-
-
 # https://codeberg.org/hjacobs/kube-ops-view/src/branch/main/deploy/rbac.yaml
 resource "kubernetes_service_account" "kube-ops-view" {
   provider = "kubernetes.azure_k8s"
